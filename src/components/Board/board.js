243f25cef8a4c87c0ddb709495754df227f345ea
OmkarBhat16 <omkar.mitmpl2022@learner.manipal.edu>
2025-03-24 13:26:16 +0530

    got pieces on board

--- src/components/Board/board.js
+++ src/components/Board/board.js
@@ -2,29 +2,40 @@ import './board.css';
 import { getCharacter } from '../../helper';
 import Files from '../bits/Files'
 import Ranks from '../bits/Ranks'
-const Board=()=>{
+import { useState, useEffect } from "react";
+const Board = () => {
 
-    const getClassName=(i,j)=>{
-        let c='tile'
-        c+=(i+j)%2===0?' tile--light':' tile--dark'
-        return c;
-    }
-     
-    const ranks=Array(8).fill().map((x,i)=>8-i)
-    const files=Array(8).fill().map((x,i)=>getCharacter(i))
+  const getClassName = (i, j) => {
+    let c = 'tile'
+    c += (i + j) % 2 === 0 ? ' tile--light' : ' tile--dark'
+    return c;
+  }
+  const mapping = {
+    "k": "black king", K: "white king",
+    "q": "black queen", Q: "white queen",
+    "b": "black bishop", B: "white bishop",
+    "n": "black knight", N: "white knight",
+    "r": "black rook", R: "white rook",
+    "p": "black pawn", P: "white pawn",
+  }
 
-    return <div className='board'>
-        <Ranks ranks={ranks}/>
+  const state = useState(["rnbkqbnr", "pppppppp", "8", "8", "8", "8", "PPPPPPPP", "RNBQKBNR"])
+  console.log(state[0][7][0])
+  const ranks = Array(8).fill().map((x, i) => 8 - i)
+  const files = Array(8).fill().map((x, i) => getCharacter(i))
 
-        <div className='tiles'>
-            {
-                ranks.map((rank,i)=>files.map((file,j)=>
-                    <div key={file+'-'+rank} className={getClassName(i,j)}></div>
-            ))
-            }
-        </div>
-        <Files files={files}/>
+  return <div className='board'>
+    <Ranks ranks={ranks} />
+
+    <div className='tiles'>
+      {
+        ranks.map((rank, i) => files.map((file, j) =>
+          <div key={file + '-' + rank} className={getClassName(i, j)}>{state[0][i][j] != 8 ? state[0][i][j] : " "}</div>
+        ))
+      }
     </div>
+    <Files files={files} />
+  </div>
 }
 
 export default Board;
